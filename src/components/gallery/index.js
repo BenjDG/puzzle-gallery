b69f7c7d7c9dc12e4a33f5d7ff1b67a3e07b70fa
Ben Galloway <bdgalloway85@gmail.com>
2021-04-18 19:28:56 -0500

    add placeholder picture

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './styles.css';
 
-export default function Gallery  {
-    return (
-      <div>
-        <h1>Test</h1>
-      </div>
-    )
+export default function Gallery () {
+  return (
+    <div>
+      <h1>Test</h1>
+      <img src='https://placekitten.com/640/360' alt='kitten' />
+    </div>
+  );
 }
