44be1343a6fcdeee2e81204d1b30514879be862e
Ben Galloway <bdgalloway85@gmail.com>
2021-04-19 15:47:13 -0500

    add express server code

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(helmet({ contentSecurityPolicy: false }));
+
+app.use(morgan('dev'));
+
+// for Reactjs ##################
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+// #################################################
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(
+    `🌎 Server is Ready and Listening on http://localhost:${PORT}`
+  ); // eslint-disable-line no-console
+});
