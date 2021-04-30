6ff50f8cb6e9f1c5503393b3bba6f1a2f4a1c52d
Ben Galloway <bdgalloway85@gmail.com>
2021-04-30 08:44:08 -0500

    add login page

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -4,7 +4,13 @@ import './styles.css';
 export default function Login () {
   return (
     <div>
-      <h1>login</h1>
+      <form>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" /><br /><br />
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" /><br /><br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
