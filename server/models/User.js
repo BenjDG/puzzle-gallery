2a116f24158e075735e503adade9737aa4a7c683
Ben Galloway <bdgalloway85@gmail.com>
2021-05-03 09:06:45 -0500

    fix User register

--- server/models/User.js
+++ server/models/User.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  username: { type: String, unique: true },
+  password: { type: String}
 });
 
 userSchema.plugin(passportLocalMongoose);
