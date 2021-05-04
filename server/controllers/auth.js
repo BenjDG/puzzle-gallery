b44ca47ddb2f423543c5e2914574798fe6a51b74
Ben Galloway <bdgalloway85@gmail.com>
2021-05-04 16:51:03 -0500

    auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,25 +1,20 @@
-const passport = require('../config/passport');
 const { User } = require('../models');
 
-const login = async function (req, res) {
-
+const register = async function (req, res) {
+  try {
+    const user = new User({ username: req.body.username });
+    await user.setPassword(req.body.password);
+    await user.save();
+  }
+  catch (err) {
+    console.log(`error inside save ${err}`);
+    res.status(500).send(err);
+  }
 };
 
-const register = async function (req, res, next) {
-  console.log('register triggered');
-  console.log(`req.body.username`, req.body.username);
-  console.log(`req.body.password`, req.body.password);
-  User.register({username: req.body.username}, req.body.password, function (err, _user){
-    if (err) {
-      console.dir(err);
-      res.send(err);
-      //const errObj = new Error(err);
-      //res.status(500).send(err);
-    } else {
-      passport.authenticate('local');
-      res.status(200);
-    }
-  })
+const login = async function (req, res) {
+  //code block under construction
+  console.log(`login!!!!!`);
 };
 
 const logout = function (req, res) {
